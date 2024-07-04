6af1771ecaa2cf2a55fe4636f27eeaa81cf92f0e
Ram Niwas Sangwan <ramnsangwan@gmail.com>
2024-07-04 17:58:27 -0500

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -15,7 +15,7 @@ const CONDENSE_TEMPLATE = `Given the following conversation and a follow up ques
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_TEMPLATE = `You are an expert researcher on Generative AI at TheSkillPedia. Use the following pieces of context to answer the question at the end.
+const QA_TEMPLATE = `You are a researcher. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context or chat history, politely respond that you are tuned to only answer questions that are related to the context.
 
